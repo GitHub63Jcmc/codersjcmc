a4ee7f09d8ffc75cb35a6b928c161d0f47e8c293
Joao Cabral de Matos Carvalho <jcmcgoojcmc@gmail.com>
2025-06-18 19:54:40 +0200

    52 - Abrir/Cerrar sidebar

--- src/layouts/admin/AdminLayout.vue
+++ src/layouts/admin/AdminLayout.vue
@@ -1,12 +1,15 @@
 <script setup>
     import { RouterView } from 'vue-router';
     import AdminNav from './components/AdminNav.vue';
-import AdminAside from './components/AdminAside.vue';
+    import AdminAside from './components/AdminAside.vue';
+    import { ref } from 'vue';
+
+    const openSidebar = ref(false);
 </script>
 
 <template>
-    <AdminNav />
-    <AdminAside />
+    <AdminNav v-model="openSidebar" />
+    <AdminAside v-model="openSidebar" />
 
     <div class="p-4 sm:ml-64">
        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
