79137c616145a85360254dafe9438c3075211e2f
Joao Cabral de Matos Carvalho <jcmcgoojcmc@gmail.com>
2025-06-20 13:39:24 +0200

    Modulo compartido lección 56 - Dropdown

--- src/layouts/public/components/PublicNav.vue
+++ src/layouts/public/components/PublicNav.vue
@@ -3,6 +3,7 @@
     import { RouterLink } from 'vue-router';
     import PublicNavLink from './PublicNavLink.vue';
     import AppLogo from '@/modules/shared/components/AppLogo.vue';
+    import DropdownMenu from '@/modules/shared/components/DropdownMenu.vue';
     const openMenu = ref(false);
 </script>
 
@@ -15,12 +16,34 @@
                 </RouterLink>
             </div>
             <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                <button type="button" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-                    <span class="sr-only">Open user menu</span>
-                    <img class="w-10 h-10 rounded-full" src="/src/assets/img/JoaoWebSite2.jpg" alt="user photo">
-                </button>
+                <DropdownMenu>
+                    <template #trigger>
+                        <button type="button" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                        <span class="sr-only">Open user menu</span>
+                        <img class="w-10 h-10 rounded-full" src="/src/assets/img/JoaoWebSite2.jpg" alt="user photo">
+                        </button>
+                    </template>
+
+                    <DropdownItem :to="{
+                        name: 'admin.dashboard'
+                    }">
+                        Admin
+                    </DropdownItem>
+
+                    <DropdownItem :to="{
+                        name: 'login'
+                    }">
+                        Iniciar sesión
+                    </DropdownItem>
+
+                    <DropdownItem :to="{
+                        name: 'register'
+                    }">
+                        Registrarse
+                    </DropdownItem>
+                </DropdownMenu>
                 <!-- Dropdown menu -->
-                <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+                <!-- <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
                     <div class="px-4 py-3">
                         <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
                         <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
@@ -39,7 +62,7 @@
                             <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
                         </li>
                     </ul>
-                </div>
+                </div> -->
                 <button @click="openMenu = !openMenu"
                     data-collapse-toggle="navbar-user" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
---
+++ src/modules/shared/components/DropdownMenu.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import { ref } from 'vue';
+    const open = ref(false);
+</script>
+
+<template>
+    <div class="relative">
+        <div>
+            <slot name="trigger"></slot>
+        </div>
+
+        <div class="absolute z-50 end-0 mt-2 w-48">
+            <div class="bg-white shadow-lg rounded-md py-1 flex flex-col">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
