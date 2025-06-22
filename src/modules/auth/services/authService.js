953f8caf3cf1d759a96222642a3fbdce795c7982
Joao Cabral de Matos Carvalho <jcmcgoojcmc@gmail.com>
2025-06-22 18:58:38 +0200

    66 - Servicios de autentificación

---
+++ src/modules/auth/services/authService.js
@@ -0,0 +1,39 @@
+import apiClient from '@/api/axiosConfig';
+
+export default {
+    async login(credencials) {
+        try {
+            const response = await apiClient.post('auth/login', credencials);
+            return response.data;
+        } catch (error) {
+            return error.response.data;
+        }
+    },
+
+    async logout() {
+        try {
+            const response = await apiClient.post('auth/logout');
+            return response.data;
+        } catch (error) {
+            return error.response.data;
+        }
+    },
+
+    async refresh() {
+        try {
+            const response = await apiClient.post('auth/refresh');
+            return response.data;
+        } catch (error) {
+            return error.response.data;
+        }
+    },
+
+    async me() {
+        try {
+            const response = await apiClient.post('auth/me');
+            return response.data;
+        } catch (error) {
+            return error.response.data;
+        }
+    },
+}
